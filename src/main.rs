b7574bbf60f279b193137ffd2d0a6572697baca0
nuzzesick <mati280341@gmail.com>
2023-01-21 01:36:04 -0300

    doing sum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+	let mut number_one : String = String::new();
+	let mut number_two : String = String::new();
+
+	println!("Type first number: ");
+
+	std::io::stdin().read_line(&mut number_one).unwrap();
+
+	println!("Type second number: ");
+
+	std::io::stdin().read_line(&mut number_two).unwrap();
+
+	let number_one_int : u8 = number_one.trim().parse().unwrap();
+
+	let number_two_int : u8 = number_two.trim().parse().unwrap(); 
+
+	println!("{}", number_one_int + number_two_int);
 }
